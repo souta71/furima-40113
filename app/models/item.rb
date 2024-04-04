b014005c0d2f687c42b13bebe8314c0df03e39da
souta71 <imakuni02@gmail.com>
2024-04-04 17:08:22 +0900

    ActiveHash

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,20 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :status
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :shipping
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :area
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :delivery
+
+  validates :category_id, numericality: { other_than: 1 }
+  validates :status_id, numericality: { other_than: 1 }
+  validates :shipping_id, numericality: { other_than: 1 }
+  validates :area_id, numericality: { other_than: 1 }
+  validates :delivery_id, numericality: { other_than: 1 }
 end
