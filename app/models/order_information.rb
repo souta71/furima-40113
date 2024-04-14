72be24d1a387daa6f42ed8df0d33da8540b709d4
souta71 <imakuni02@gmail.com>
2024-04-14 23:47:46 +0900

    モデル、アソシエーション作成

---
+++ app/models/order_information.rb
@@ -0,0 +1,19 @@
+class OrderInformation
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :area_id, :city, :address, :building, :phone_number, :order_id
+
+  validates :user_id, presence: true
+  validates :item_id, presence: true
+  validates :postal_code, presence: true, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
+  validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :city, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true, numericality: { only_integer: true }, length: { in: 10..11 }
+  validates :order_id, presence: true
+
+  def save
+    Order.create(user_id: user_id, item_id: item_id)
+
+    Information.create(postal_code: postal_code, area_id: area_id, city: city, address: address, phone_number: phone_number, order_id: order_id )
+  end
+end
